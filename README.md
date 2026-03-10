ec5870904f397a02001a8ca27e0ace32545f5f55
William <V1270077727@gmail.com>
2026-03-10 20:28:51 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Attack-Framework
+A collection of simulated malicious websites for testing the autofill feature of web-based password managers.
---
+++ common.js
@@ -0,0 +1,33 @@
+// activate the hidden template field
+function activateTemplateField(formId, templateId = 'hiddenTemplate') {
+    const template = document.getElementById(templateId);
+    if (!template) {
+        console.error('Template container not found:', template);
+        return;
+    }
+    const clone = template.content.cloneNode(true);
+    const input = clone.querySelector('input');
+    // append it to the form, making it part of the DOM
+    document.getElementById(formId).appendChild(input);
+    return input; // return the newly inserted input element
+}
+
+// clear form data
+function onRefresh() {
+    const baseUrl = window.location.origin + window.location.pathname;
+    window.location.replace(baseUrl);
+}
+
+// display test results
+function displayTestResults(containerId = 'resultsContent', title, resultMessage, resultColor) {
+    const container = document.getElementById(containerId);
+    if (!container) {
+        console.error('Test results container not found:', containerId);
+        return;
+    }
+    const contentDiv = container.querySelector('#resultsContent') || container;
+    contentDiv.innerHTML = resultMessage;
+    container.style.backgroundColor = resultColor;
+    container.style.display = 'block';
+    container.scrollIntoView({behavior: 'smooth'});
+}
